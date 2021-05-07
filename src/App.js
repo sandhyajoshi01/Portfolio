63ace709d0f2c679464e50833174e01edd06e511
Sandhya Joshi <sandhyajoshi01@gmail.com>
2021-05-06 23:59:52 -0600

    fixed conflict errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import MainPage from './Components/MainPage';
 import {FooterComp} from './Components/Footer';
-<<<<<<< HEAD
 import {Navigation} from "./Components/Navigation";
 import Sandhya from "./Images/MainPage.JPG";
 
@@ -21,14 +20,6 @@ function App() {
         <Navigation backgroundImage={Sandhya}/>
         <MainPage/>
         <FooterComp backgroundImage={Sandhya}/>
-=======
-
-function App() {
-  return (
-    <div className="App">
-      <MainPage/>
-        <FooterComp/>
->>>>>>> 9da6d4f1688dadac8e4434d189594a97bdb332a2
     </div>
   );
 }
